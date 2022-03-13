417ce0035b767961a1d746c355f95700d24fae63
mwaidesu <machariamwai@gmail.com>
2022-03-13 18:09:07 +0300

    Change html file

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/* var submitForm = function(Event){
+  Event.preventDefault();
+
+  var formName = document.getElementById("name").value;
+  console.log(formName);
+  var formEmail = document.getElementById("email").value;
+  console.log(formEmail);
+  var formMessage = document.getElementById("message").value;
+  console.log(formMessage);
+
+  if (formName.value !== null && formEmail.value !== null && formMessage.value !== null){
+    alert("We have received your message. Thank you.")
+  }
+
+}
+
+function checkSubmit(e) {
+  if(e && e.keyCode == 13) {
+     document.forms[0].submit();
+  }
+} */
